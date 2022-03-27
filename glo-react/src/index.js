56bd8d57dbbd4454b301fba2ee3e6b5cb4ec066d
LizaKhvesko <khveskoliza@tut.by>
2022-03-27 19:08:31 +0300

    use create React

---
+++ glo-react/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/style.css';
+import BusinessCard from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BusinessCard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
